bbb748bb42d4ba6400c185159273ef4d0e84d778
hhhaunteddd <mallowthepuff@gmail.com>
2022-11-13 12:59:11 -0600

    cleaned up queries.sql

--- queries.sql
+++ queries.sql
@@ -5,10 +5,14 @@ name varchar,
 skills varchar
 
 -- Part 2: Test it with SQL
-SELECT name FROM employer WHERE location = "St. Louis City";
+SELECT name FROM employer
+WHERE location = "St. Louis City";
 
 -- Part 3: Test it with SQL
 DROP TABLE job;
 
 -- Part 4: Test it with SQL
-SELECt * FROM skill INNER JOIN job_skills ON skill.id = job_skills.skills_id WHERE job_skills.jobs_id IS NOT NULL ORDER BY name ASC;
+SELECt * FROM skill
+INNER JOIN job_skills ON skill.id = job_skills.skills_id
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY name ASC;
